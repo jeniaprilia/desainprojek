a4a73aaa6cea08d38d2ad162de8ad9e868d74f16
jeniaprilia <38410998+jeniaprilia@users.noreply.github.com>
2018-07-21 11:29:41 +0700

    Update harga.php

--- harga.php
+++ harga.php
@@ -66,7 +66,7 @@
 		<div class="container">
 			<nav class="navbar navbar-expand-lg navbar-light mu-navbar">
 				<!-- Text based logo -->
-				<a class="navbar-brand mu-logo" href="index.html"><span>B-HERO</span></a>
+				<a class="navbar-brand mu-logo" href="index.html"><span>KangenNet</span></a>
 				<!-- image based logo -->
 			   	<!-- <a class="navbar-brand mu-logo" href="index.html"><img src="assets/images/logo.png" alt="logo"></a> -->
 			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -75,23 +75,13 @@
 
 			  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 			    <ul class="navbar-nav mr-auto mu-navbar-nav">
-			      <li class="nav-item">
-			        <a href="index.html">Home</a>
-			      </li>
-			      <li class="nav-item"><a href="about-us.html">About us</a></li>
-			      <li class="nav-item active"><a href="services.html">Services</a></li>
-			      <li class="nav-item"><a href="portfolio.html">Portfolio</a></li>
-			       <li class="nav-item dropdown">
-				        <a class="dropdown-toggle" href="blog.html" role="button" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-				          Blog
-				        </a>
-				       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-				          <a class="dropdown-item" href="blog.html">Blog Archive</a>
-				          <a class="dropdown-item" href="blog-single.html">Blog Single</a>
-				        </div>
-				    </li>
-			        <li class="nav-item"><a href="contact.html">Contact us</a></li>
-			        <li class="nav-item"><a href="404.html">404 Page</a></li>
+				<li class="nav-item"><a href="index.php">Beranda</a></li>
+			      <li class="nav-item active">
+			        <a href="harga.php">Harga Paket</a>
+			      </li>						     
+			        <li class="nav-item"><a href="bantuan.php">Bantuan</a></li>
+					 <li class="nav-item"><a href="tentang.php">Tentang Kami</a></li>
+			        
 			    </ul>
 			  </div>
 			</nav>
